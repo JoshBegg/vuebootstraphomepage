09cce8462c716273e4c0dc201adb27fc2e7dc5e7
JoshBegg <93288205+JoshBegg@users.noreply.github.com>
2022-03-10 14:45:22 -0700

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,75 @@
+<template>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="mb-auto">
+        <div>
+          <h3 class="float-md-start mb-0">Welcome</h3>
+          <nav class="nav nav-masthead justify-content-center float-md-end">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Contact</a>
+          </nav>
+        </div>
+      </header>
+
+      <main class="px-3">
+        <h1>Ligula ullamcorper</h1>
+        <p class="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed odio
+          morbi quis commodo. At lectus urna duis convallis convallis tellus id
+          interdum velit. Tincidunt dui ut ornare lectus sit. Nascetur ridiculus
+          mus mauris vitae ultricies leo. Ullamcorper sit amet risus nullam.
+          Fermentum leo vel orci porta. Imperdiet nulla malesuada pellentesque
+          elit eget gravida cum sociis. Enim sed faucibus turpis in. Tellus in
+          metus vulputate eu scelerisque felis imperdiet proin. Tincidunt
+          praesent semper feugiat nibh sed pulvinar proin gravida hendrerit.
+          Lacus laoreet non curabitur gravida. Elit at imperdiet dui accumsan
+          sit amet nulla facilisi. At elementum eu facilisis sed odio morbi quis
+          commodo. Ut porttitor leo a diam. Laoreet id donec ultrices tincidunt
+          arcu. Sed faucibus turpis in eu mi bibendum neque egestas. Ut etiam
+          sit amet nisl.
+        </p>
+        <p class="lead">
+          <a
+            href="#"
+            class="btn btn-lg btn-secondary fw-bold border-white black"
+            >Learn more</a
+          >
+        </p>
+      </main>
+
+      <footer class="mt-auto text-white-50">
+        <p>
+          Cover template for
+          <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>,
+          by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.
+        </p>
+      </footer>
+    </div>
+  </body>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+</style>
